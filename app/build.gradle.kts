aeda432d609bd695cb68d1fae285b9524fb096ec
Grigroviska <karagozoglualper@protonmail.com>
2024-03-10 00:17:42 +0300

    The basis of Nopedot was created.

---
+++ app/build.gradle.kts
@@ -0,0 +1,104 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp")
+}
+
+android {
+    namespace = "com.grigroviska.nopedot"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.grigroviska.nopedot"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+
+    implementation("com.google.android.material:material:1.11.0")
+    val lifecycle_version = "2.7.0"
+    val room_version = "2.6.1"
+    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
+
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+
+    // RecyclerView Animator
+    implementation("jp.wasabeef:recyclerview-animators:4.0.2")
+
+    // ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:$lifecycle_version")
+
+
+    // LiveData
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycle_version")
+
+    // Annotation processor
+    implementation("androidx.lifecycle:lifecycle-common-java8:2.7.0")
+
+    //Room
+    implementation("androidx.room:room-runtime:$room_version")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.23")
+    ksp("androidx.room:room-compiler:$room_version")
+
+    //Kotlin Extensions and Coroutines support for Room
+    implementation("androidx.room:room-ktx:$room_version")
+
+    // Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.8.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.8.0")
+
+    // Navigation Components
+    implementation("androidx.fragment:fragment-ktx:1.6.2")
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
+
+    //material Components
+    implementation("com.google.android.material:material:1.11.0")
+
+    //color picker library
+    implementation("com.thebluealliance:spectrum:0.7.1")
+
+    implementation("io.github.yahiaangelo.markdownedittext:markdownedittext:1.1.3")
+    implementation("io.noties.markwon:core:4.6.2")
+    implementation("io.noties.markwon:ext-strikethrough:4.6.2")
+    implementation("io.noties.markwon:ext-tasklist:4.6.2")
+
+}
---
+++ app/src/main/java/com/grigroviska/nopedot/HomeScreen.kt
@@ -0,0 +1,54 @@
+package com.grigroviska.nopedot
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
+import com.google.android.material.tabs.TabLayout
+import com.grigroviska.nopedot.databinding.ActivityHomeScreenBinding
+
+class HomeScreen : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomeScreenBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHomeScreenBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val viewPager = binding.viewPager
+        val tabLayout = binding.tabLayout
+
+        val pagerAdapter = ViewPagerAdapter(supportFragmentManager, lifecycle)
+
+        tabLayout.addTab(tabLayout.newTab().setText("Notes"))
+        tabLayout.addTab(tabLayout.newTab().setText("Tasks"))
+
+        viewPager.adapter = pagerAdapter
+
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+                if (tab != null){
+                    viewPager.currentItem = tab.position
+                }
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+
+            }
+
+
+        })
+
+        viewPager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+            override fun onPageSelected(position: Int) {
+                super.onPageSelected(position)
+                tabLayout.selectTab(tabLayout.getTabAt(position))
+            }
+        })
+    }
+}
