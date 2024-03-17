0a8ef7d9f6d8297d9026027618b8f90d797278da
Grigroviska <karagozoglualper@protonmail.com>
2024-03-17 03:11:22 +0300

    Topbar added to HomeScreen

--- app/src/main/java/com/grigroviska/nopedot/activities/HomeScreen.kt
+++ app/src/main/java/com/grigroviska/nopedot/activities/HomeScreen.kt
@@ -3,7 +3,14 @@ package com.grigroviska.nopedot.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.grigroviska.nopedot.R
 import com.grigroviska.nopedot.databinding.ActivityHomeScreenBinding
 import com.grigroviska.nopedot.db.NoteDatabase
 import com.grigroviska.nopedot.repository.NoteRepository
@@ -18,19 +25,22 @@ class HomeScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         supportActionBar?.hide()
-        binding = ActivityHomeScreenBinding.inflate(layoutInflater)
-        val view = binding.root
+
 
         try {
+            binding = ActivityHomeScreenBinding.inflate(layoutInflater)
+            val view = binding.root
             setContentView(view)
             val noteRepository = NoteRepository(NoteDatabase(this))
             val noteActivityViewModelFactory = NoteActivityViewModelFactory(noteRepository)
             noteActivityViewModel = ViewModelProvider(this,
                 noteActivityViewModelFactory)[NoteActivityViewModel::class.java]
+
         }catch (e: Exception){
 
             Log.d("TAG","ERROR")
 
         }
+
     }
 }
