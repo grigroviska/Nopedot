d0a725995d3f1f5a63efb4a3a2ccb420b2a8720c
Grigroviska <karagozoglualper@protonmail.com>
2024-04-22 00:23:19 +0300

    Added task deleting and searching feature.

--- app/src/main/java/com/grigroviska/nopedot/fragments/TaskFeedFragment.kt
+++ app/src/main/java/com/grigroviska/nopedot/fragments/TaskFeedFragment.kt
@@ -14,22 +14,28 @@ import android.view.ViewGroup
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.LinearLayout
+import android.widget.SearchView
 import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.content.res.AppCompatResources
+import androidx.core.content.ContextCompat
 import androidx.core.view.isVisible
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.Navigation
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.StaggeredGridLayoutManager
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.button.MaterialButton
+import com.google.android.material.snackbar.BaseTransientBottomBar
+import com.google.android.material.snackbar.Snackbar
 import com.grigroviska.nopedot.R
 import com.grigroviska.nopedot.activities.HomeScreen
 import com.grigroviska.nopedot.adapters.RvNotesAdapter
 import com.grigroviska.nopedot.adapters.RvTasksAdapter
 import com.grigroviska.nopedot.databinding.FragmentTaskFeedBinding
 import com.grigroviska.nopedot.model.Task
+import com.grigroviska.nopedot.utils.SwipeToDelete
 import com.grigroviska.nopedot.utils.hideKeyboard
 import com.grigroviska.nopedot.viewModel.TaskActivityViewModel
 import java.text.SimpleDateFormat
@@ -66,6 +72,8 @@ class TaskFeedFragment : Fragment() {
         val activity= activity as HomeScreen
         val navController= Navigation.findNavController(view)
         requireView().hideKeyboard()
+
+        swipeToDelete(binding.rvTask)
         try{
         recyclerViewDisplay()}
         catch (e: Exception){
@@ -76,6 +84,38 @@ class TaskFeedFragment : Fragment() {
         binding.floatingActionButton.setOnClickListener {
             showBottomSheet()
         }
+
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener,
+            androidx.appcompat.widget.SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+
+                if (!query.isNullOrEmpty()) {
+                    val searchText = "%$query"
+                    taskActivityViewModel.searchTask(searchText).observe(viewLifecycleOwner) { notes ->
+                        rvAdapter.submitList(notes)
+                    }
+                } else {
+                    observerDataChanges()
+                }
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                if (newText.isNullOrEmpty()) {
+                    observerDataChanges()
+                } else {
+                    val searchText = "%$newText"
+                    taskActivityViewModel.searchTask(searchText).observe(viewLifecycleOwner) { notes ->
+                        rvAdapter.submitList(notes)}
+                }
+                return true
+            }
+        })
+
+        binding.searchView.setOnSearchClickListener {
+            requireView().hideKeyboard()
+        }
+
     }
 
     private fun showBottomSheet(){
@@ -168,6 +208,7 @@ class TaskFeedFragment : Fragment() {
 
                     val task = Task(0,newTask.text.toString(), getOpenedEditTexts(), getOpenedEditTexts(), selectedLastDate)
                     taskActivityViewModel.saveTask(task)
+                    dialog.dismiss()
                 }
             }catch (e: Exception){
 
@@ -176,10 +217,58 @@ class TaskFeedFragment : Fragment() {
 
             }
 
-
         }
 
         dialog.show()
+    }
+
+    private fun swipeToDelete(rvNote: RecyclerView) {
+
+        val swipeToDeleteCallback = object : SwipeToDelete(){
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                val position = viewHolder.absoluteAdapterPosition
+                val task= rvAdapter.currentList[position]
+                var actionBtnTapped= false
+                taskActivityViewModel.deleteTask(task)
+                binding.searchView.apply {
+                    hideKeyboard()
+                    clearFocus()
+                }
+                if (binding.searchView.toString().isEmpty()){
+                    observerDataChanges()
+
+                }
+                val snackBar= Snackbar.make(
+                    requireView(), "Task Deleted!", Snackbar.LENGTH_LONG
+                ).addCallback(object : BaseTransientBottomBar.BaseCallback<Snackbar>(){
+                    override fun onDismissed(transientBottomBar: Snackbar?, event: Int) {
+                        super.onDismissed(transientBottomBar, event)
+                    }
+
+                    override fun onShown(transientBottomBar: Snackbar?) {
+
+                        transientBottomBar?.setAction("UNDO"){
+                            taskActivityViewModel.saveTask(task)
+                            actionBtnTapped=true
+                        }
+
+                        super.onShown(transientBottomBar)
+                    }
+                }).apply {
+                    animationMode= Snackbar.ANIMATION_MODE_FADE
+                    setAnchorView(R.id.floatingActionButton)
+                }
+                snackBar.setActionTextColor(
+                    ContextCompat.getColor(
+                        requireContext(),
+                        R.color.yellowOrange
+                    )
+                )
+                snackBar.show()
+            }
+        }
+        val itemTouchHelper= ItemTouchHelper(swipeToDeleteCallback)
+        itemTouchHelper.attachToRecyclerView(rvNote)
     }
 
     private fun getOpenedEditTexts(): MutableList<String> {
